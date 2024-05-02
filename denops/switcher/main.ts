c950989ebd1d5784a45f5ad164dc454fdf2e82e2
nekowasabi <nolifeking00@gmail.com>
2024-05-02 19:00:47 +0900

    feat: 動くところまで実装 :tada:

--- denops/switcher/main.ts
+++ denops/switcher/main.ts
@@ -1,9 +1,10 @@
 import { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
-import * as n from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
-import * as v from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
-import { feedkeys } from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
+// import * as n from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
+// import * as v from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
+// import {feedkeys} from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
+// import { realPathSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
   async function getCurrentFilePath(): Promise<string> {
@@ -12,8 +13,33 @@ export async function main(denops: Denops): Promise<void> {
 
   denops.dispatcher = {
     async switchByRule(): Promise<void> {
-      console.log(getCurrentFilePath());
-      console.log(await denops.cmd("echo 'Hello, world!'"));
+      const files = [
+        "~/.config/nvim/init.vim",
+        "~/.config/nvim/rc/plugin.vim",
+      ];
+
+      const homeDirectroy = ensure(Deno.env.get("HOME"), is.String);
+
+      const absolutePaths = files.map((file: string) => {
+        const filePath = file.replace("~", homeDirectroy);
+        return Deno.realPathSync(filePath);
+      });
+
+      // 現在のファイルパスを取得（絶対パス）
+      const currentFilePath = Deno.realPathSync(await getCurrentFilePath());
+
+      // ファイルパスが配列に含まれているか確認
+      const isInclude = absolutePaths.includes(currentFilePath);
+
+      // 含まれていた場合、次のファイルパスを取得
+      if (isInclude) {
+        const nextFilePath = absolutePaths[
+          (absolutePaths.indexOf(currentFilePath) + 1) % absolutePaths.length
+        ];
+
+        // 取得したファイルパスを開く
+        await denops.cmd(`:e ${nextFilePath}`);
+      }
     },
   };
 
