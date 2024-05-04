7d0c2b32d639bda7d67a5505dfc4be0fa7032dd3
nekowasabi <nolifeking00@gmail.com>
2024-05-04 11:32:25 +0900

    feat: スイッチャーの条件を読み込む

--- denops/switcher/main.ts
+++ denops/switcher/main.ts
@@ -1,40 +1,104 @@
 import { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
-// import * as n from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
-// import * as v from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
+import * as v from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
-// import {feedkeys} from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
-// import { realPathSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
+  /**
+   * Returns the current file path.
+   *
+   * @returns {Promise<string>} The current file path.
+   */
   async function getCurrentFilePath(): Promise<string> {
     return ensure(await fn.expand(denops, "%:p"), is.String);
   }
 
+  /**
+   * ファイル名から共通部分を取得する
+   * @param fileName ファイル名
+   * @param condition 条件
+   * @returns 共通部分
+   */
+  function getCommonPart(fileName: string, condition: Condition): string {
+    if (condition.postfix && fileName.endsWith(condition.postfix)) {
+      fileName = fileName.replace(condition.postfix, "");
+    }
+    if (condition.prefix && fileName.startsWith(condition.prefix)) {
+      fileName = fileName.replace(condition.prefix, "");
+    }
+    return fileName;
+  }
+
+  type Condition = {
+    path: string[];
+    rule: string;
+    postfix?: string;
+    prefix?: string;
+  };
+
   denops.dispatcher = {
     async switchByRule(): Promise<void> {
-      const files = [
-        "~/.config/nvim/init.vim",
-        "~/.config/nvim/rc/plugin.vim",
-      ];
+      const switchers = ensure(
+        await v.g.get(denops, "switch_rule"),
+        // jsonの形式を模倣して型を判定する
+        is.ObjectOf({
+          conditions: is.ArrayOf(
+            is.ObjectOf({
+              rule: is.String,
+              path: is.ArrayOf(is.String),
+            }),
+          ),
+        }),
+      );
 
       const homeDirectroy = ensure(Deno.env.get("HOME"), is.String);
 
-      const absolutePaths = files.map((file: string) => {
-        const filePath = file.replace("~", homeDirectroy);
-        return Deno.realPathSync(filePath);
-      });
+      // pathの中に~が含まれている場合は、それをホームディレクトリに置き換える
+      // switchers.conditionsの中身を書き換える
+      const fileName = ensure(await fn.expand(denops, "%:t:r"), is.String);
+      const replacedConditions = switchers.conditions.map(
+        (condition: Condition) => {
+          // 無名関数にして処理をまとめる
+          const realPath = (path: string) => {
+            if (path.includes("%")) {
+              path = path.replace("%", getCommonPart(fileName, condition));
+            }
+            return path.replace("~", homeDirectroy);
+          };
 
+          return {
+            // jsonを組み立てればいいのか
+            path: condition.path.map(realPath),
+            rule: condition.rule,
+          };
+        },
+        fileName,
+      );
       const currentFilePath = Deno.realPathSync(await getCurrentFilePath());
-      const isInclude = absolutePaths.includes(currentFilePath);
 
-      if (isInclude) {
-        const nextFilePath = absolutePaths[
-          (absolutePaths.indexOf(currentFilePath) + 1) % absolutePaths.length
-        ];
+      const condition: Condition | undefined = replacedConditions.find((
+        c: Condition,
+      ) => c.path.includes(currentFilePath));
 
+      if (!condition) {
+        return;
+      }
+
+      // ruleに沿ってパスを取得する
+      if (condition.rule === "file") {
+        const nextFilePathIndex =
+          (condition.path.indexOf(currentFilePath) + 1) %
+          condition.path.length;
+        const nextFilePath = condition.path[nextFilePathIndex];
         await denops.cmd(`:e ${nextFilePath}`);
       }
+      // // rule: gitの場合は、git ls-filesで取得して、最初の候補を取得する（候補が複数ある場合は、ddu起動予定）
+      // if (condition.rule === "git") {
+      //   // git ls-filesで取得したファイル一覧を取得する
+      //   const f = await fn.system(denops, "git ls-files");
+      //   // 改行で分割して、配列にする
+      //   const files = f.trim().split("\n");
+      // }
     },
   };
 
