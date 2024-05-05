54d20d22178b5be5092df2c3702045c04a683c0a
nekowasabi <nolifeking00@gmail.com>
2024-05-06 06:32:08 +0900

    feat: Integrate ddu. :tada:

--- denops/@ddu-sources/rule_switch.ts
+++ denops/@ddu-sources/rule_switch.ts
@@ -6,7 +6,33 @@ import {
 } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 import { Denops } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
+import { Condition, getSwitcherRule } from "../switcher/common.ts";
+import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
+
 type Params = Record<never, never>;
+
+function getFiles(switcher: Condition): string[] {
+  return switcher.path.map((path: string) => {
+    return path;
+  });
+}
+
+async function getGitFiles(
+  denops: Denops,
+  switcher: Condition,
+  result: Array<string>,
+): Promise<string[]> {
+  const gitRoot = (await fn.system(denops, "git rev-parse --show-toplevel"))
+    .trim();
+
+  return switcher.path
+    .flatMap((file: string) =>
+      result
+        .filter((r: string) => r.includes(file))
+        .map((r: string) => `${gitRoot}/${r}`)
+    );
+}
 
 export class Source extends BaseSource<Params> {
   override kind = "rule_switch";
@@ -21,10 +47,27 @@ export class Source extends BaseSource<Params> {
     return new ReadableStream<Item<ActionData>[]>({
       async start(controller) {
         try {
-          const result = await args.denops.call("system", "git ls-files");
-          const files = result.split("\n").filter((file: string) =>
+          const result = ensure(
+            await args.denops.call("system", "git ls-files"),
+            is.String,
+          );
+          const filteredResult = result.split("\n").filter((file: string) =>
             file !== ""
           );
+
+          const type = result.includes("fatal: not a git repository") ||
+              result.includes("not a git repository")
+            ? "file"
+            : "git";
+
+          const switcher: Condition | undefined = await getSwitcherRule(
+            args.denops,
+            type,
+          );
+
+          const files = type === "file"
+            ? getFiles(switcher)
+            : await getGitFiles(args.denops, switcher, filteredResult);
           const items: Item<ActionData>[] = files.map((file: string) => ({
             word: file,
             action: {
