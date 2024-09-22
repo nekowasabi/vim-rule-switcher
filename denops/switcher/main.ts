506555c94349b86b1185813a65f9dcb82dc5038f
nekowasabi <nolifeking00@gmail.com>
2024-09-22 15:25:48 +0900

    feat: 新しいスイッチルール保存機能を追加 🆕

--- denops/switcher/main.ts
+++ denops/switcher/main.ts
@@ -10,6 +10,19 @@ import * as v from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
+    async saveSwitchRule(name: unknown): Promise<void> {
+      if (!v.g.get(denops, "switch_rule")) {
+        console.log("No switch rule found.");
+        return;
+      }
+
+      const json_path = ensure(
+        await v.g.get(denops, "switch_rule"),
+        is.String,
+      );
+      const rule_string = await Deno.readTextFile(json_path);
+    },
+
     async switchByRule(type: unknown): Promise<boolean> {
       try {
         const switcher: Condition | undefined = await getSwitcherRule(
@@ -43,5 +56,9 @@ export async function main(denops: Denops): Promise<void> {
 
   await denops.cmd(
     `command! -nargs=0 OpenSwitchRuleFile call denops#notify("${denops.name}", "openSwitchRuleFile", [])`,
+  );
+
+  await denops.cmd(
+    `command! -nargs=1 SaveSwitchRule call denops#notify("${denops.name}", "saveSwitchRule", [<f-args>])`,
   );
 }
